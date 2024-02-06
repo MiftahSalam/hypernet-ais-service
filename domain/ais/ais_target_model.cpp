ba30e60338b619ddc696693e59deb29d4316a729
miftah <salam.miftah@gmail.com>
2024-02-06 19:33:18 +0700

    adjust ais target definition

--- domain/ais/ais_target_model.cpp
+++ domain/ais/ais_target_model.cpp
@@ -1,13 +1,69 @@
 #include "ais_target_model.h"
 
-AISTargetModel::AISTargetModel(const AISTargetModel &track)
+AISTargetModel::AISTargetModel()
 {
-    this->id = track.id;
-    this->rng = track.rng;
-    this->brn = track.brn;
-    this->spd = track.spd;
-    this->crs = track.crs;
-    this->lat = track.lat;
-    this->lon = track.lon;
-    this->alt = track.alt;
+    IMO = 0;
+    MMSI = 0;
+    Class = 0;
+    VendorID = "";
+    ShipType = 19; //unknown
+    ShipName = "";
+    ShipNameExtension = "";
+    CallSign = "";
+    NavStatus = 15; //unknown
+    Lat = 0.;
+    Lon = 0.;
+    SOG = 555.;
+    COG = 666.;
+    HDG = 511.;
+    ROTAIS = -128;
+    ROTIND = 0;
+    DimA = DimB = DimC = DimD = 0;
+    Draft = 0.;
+    Euro_Length = 0;
+    Euro_Beam = 0;
+    Euro_Draft = 0;
+    Euro_VIN = "";
+    UN_shiptype = 0;
+    ETA_Mo = 0;
+    ETA_Day = 0;
+    ETA_Hr = 24;
+    ETA_Min = 60;
+    Destination = "";
+}
+
+AISTargetModel::AISTargetModel(const AISTargetModel &target)
+{
+    IMO = target.IMO;
+    MMSI = target.MMSI;
+    Class = target.Class;
+    VendorID = target.VendorID;
+    ShipType = target.ShipType;
+    ShipName = target.ShipName;
+    ShipNameExtension = target.ShipNameExtension;
+    CallSign = target.CallSign;
+    NavStatus = target.NavStatus;
+    Lat = target.Lat;
+    Lon = target.Lon;
+    SOG = target.SOG;
+    COG = target.COG;
+    HDG = target.HDG;
+    ROTAIS = target.ROTAIS;
+    ROTIND = target.ROTIND;
+    DimA = target.DimA;
+    DimB = target.DimB;
+    DimC = target.DimC;
+    DimD = target.DimD;
+    Draft = target.Draft;
+    Euro_Length = target.Euro_Length;
+    Euro_Beam = target.Euro_Beam;
+    Euro_Draft = target.Euro_Draft;
+    Euro_VIN = target.Euro_VIN;
+    UN_shiptype = target.UN_shiptype;
+    ETA_Mo = target.ETA_Mo;
+    ETA_Day = target.ETA_Day;
+    ETA_Hr = target.ETA_Hr;
+    ETA_Min = target.ETA_Min;
+    Destination = target.Destination;
+
 }
--- domain/ais/ais_target_model.h
+++ domain/ais/ais_target_model.h
@@ -1,20 +1,48 @@
 #ifndef AIS_TARGET_MODEL_H
 #define AIS_TARGET_MODEL_H
 
+#include <string>
+
 class AISTargetModel
 {
 public:
-    AISTargetModel() {}
-    AISTargetModel(const AISTargetModel &track);
+    AISTargetModel();
+    AISTargetModel(const AISTargetModel &target);
 
-    int id;
-    double lat;
-    double lon;
-    double alt;
-    double rng;
-    double brn;
-    double spd;
-    double crs;
+    int                       MMSI;
+    int                       IMO;
+    unsigned char             Class;
+    std::string               VendorID;
+    unsigned char             ShipType;
+    std::string               ShipName;
+    std::string               ShipNameExtension;
+    std::string               CallSign;
+    int                       NavStatus;
+    double                    Lat;
+    double                    Lon;
+    double                    SOG;
+    double                    HDG;
+    double                    COG;
+    int                       ROTAIS;
+    int                       ROTIND;
+    int                       DimA;
+    int                       DimB;
+    int                       DimC;
+    int                       DimD;
+    double                    Draft;
+    double                    Euro_Length;
+    double                    Euro_Beam;
+    double                    Euro_Draft;
+    std::string               Euro_VIN;
+    int                       UN_shiptype;
+    int                       ETA_Mo;
+    int                       ETA_Day;
+    int                       ETA_Hr;
+    int                       ETA_Min;
+    int                       ETA_Simulation;
+    std::string               Destination;
+    unsigned long long        PositionReportTicks;
+    unsigned long long        StaticReportTicks;
 };
 
 #endif // AIS_TARGET_MODEL_H
