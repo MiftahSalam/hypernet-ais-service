7178e2adc5a82f235ca8549c63c97bf11945d9d2
miftah <salam.miftah@gmail.com>
2024-02-07 18:42:21 +0700

    remove unused

--- usecase/stream/devicewrapper.cpp
+++
@@ -1,8 +0,0 @@
-#include "devicewrapper.h"
-
-DeviceWrapper::DeviceWrapper(QObject *parent) :
-    QObject(parent)
-{
-//    _currentData = "";
-    last_data_time = QDateTime::currentSecsSinceEpoch();
-}
